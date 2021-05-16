49b528726b2cf8bf79a0d90d447a94c1d6406d8f
David1105 <42513699+David1105@users.noreply.github.com>
2021-05-16 20:07:33 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,74 @@
+/* eslint no-alert: 0 */
+
+'use strict';
+
+//
+// Here is how to define your module
+// has dependent on mobile-angular-ui
+//
+var app = angular.module('MobileNotes', [
+    'ngRoute',
+    'mobile-angular-ui',
+
+    // touch/drag feature: this is from 'mobile-angular-ui.gestures.js'.
+    // This is intended to provide a flexible, integrated and and
+    // easy to use alternative to other 3rd party libs like hammer.js, with the
+    // final pourpose to integrate gestures into default ui interactions like
+    // opening sidebars, turning switches on/off ..
+    'mobile-angular-ui.gestures'
+]);
+
+app.run(function ($transform) {
+    window.$transform = $transform;
+});
+
+//
+// You can configure ngRoute as always, but to take advantage of SharedState location
+// feature (i.e. close sidebar on backbutton) you should setup 'reloadOnSearch: false'
+// in order to avoid unwanted routing.
+//
+app.config(function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'home.html',
+        reloadOnSearch: false
+    });
+    $routeProvider.when('/forms', {
+        templateUrl: 'forms.html',
+        reloadOnSearch: false
+    });
+});
+
+
+//
+// For this trivial demo we have just a unique MainController
+// for everything
+//
+app.controller('MainController', function ($rootScope, $scope) {
+
+    $scope.swiped = function (direction) {
+        alert('Swiped ' + direction);
+    };
+
+    // User agent displayed in home page
+    $scope.userAgent = navigator.userAgent;
+
+    // Needed for the loading screen
+    $rootScope.$on('$routeChangeStart', function () {
+        $rootScope.loading = true;
+    });
+
+    $rootScope.$on('$routeChangeSuccess', function () {
+        $rootScope.loading = false;
+    });
+
+    //
+    // 'Forms' screen
+    //
+    $scope.rememberMe = true;
+    $scope.email = '';
+
+    $scope.login = function () {
+        alert('Вы заполнили все поля');
+    };
+
+});
