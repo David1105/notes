2bc3518c80a5a9d02a7a8e35eff083d2e8c82434
David1105 <42513699+David1105@users.noreply.github.com>
2021-06-02 21:35:30 +0300

    Update install.php

--- install.php
+++ install.php
@@ -1,22 +1,24 @@
 <?php
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$db = "webtech"; // название таблицы
-$mysqli = mysqli_connect($servername, $username, $password, $db);
-$mysqli->set_charset("utf8");
-$mysqli->query("CREATE TABLE users (
-uid INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-mail VARCHAR(32) NOT NULL,
-pass VARCHAR(32) NOT NULL
-)");
-$mysqli->query("CREATE TABLE notes (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-uid INT(6) NOT NULL,
-mid INT(6) NOT NULL,
-folder INT(1) NOT NULL,
-note TEXT NOT NULL,
-date DATETIME
-)");
-$mysqli->close();
+  $servername = "127.0.0.1";
+  $username = "root";
+  $password = "";
+  $db = "webtech"; // название таблицы
+  $mysqli = mysqli_connect($servername, $username, $password, $db);
+
+  $mysqli->set_charset("utf8");
+  $mysqli->query("CREATE TABLE users (
+  uid INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  mail VARCHAR(32) NOT NULL,
+  pass VARCHAR(32) NOT NULL
+  )");
+
+  $mysqli->query("CREATE TABLE notes (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  uid INT(6) NOT NULL,
+  mid INT(6) NOT NULL,
+  folder INT(1) NOT NULL,
+  note TEXT NOT NULL,
+  date DATETIME
+  )");
+  $mysqli->close();
 ?>
